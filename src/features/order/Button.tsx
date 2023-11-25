b7b3a5195c24690e75031443209ae68590abd18c
Julien <hengjchan@gmail.com>
2023-11-25 19:23:26 +0800

    installing app

--- src/features/order/Button.tsx
+++ src/features/order/Button.tsx
@@ -1,9 +1,22 @@
+import { Link } from 'react-router-dom';
+
 type Props = {
   children: React.ReactNode;
   isSubmitting: boolean;
+  to: string;
 };
 
-export default function Button({ children, isSubmitting }: Props) {
+export default function Button({ children, isSubmitting, to }: Props) {
+  const className =
+    'inline-block rounded-full bg-yellow-500 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
     <button
       disabled={isSubmitting}
