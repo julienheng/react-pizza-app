8f670eb8c9b41bcb2a0a7a2a2474b323601106fe
Julien <hengjchan@gmail.com>
2023-11-26 10:18:18 +0800

    installing app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
-import { nextui } from "@nextui-org/react";
+import { nextui } from '@nextui-org/react';
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+    extend: {
+      fontSize: {
+        huge: ['80rem', { lineHeight: '1' }],
+      },
+      height: { screen: '100dvh' },
+    },
   },
   plugins: [nextui()],
 };
