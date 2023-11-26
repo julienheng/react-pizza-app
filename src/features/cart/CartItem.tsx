74f2f21e9101d434b067a78317e9f108fee3d511
Julien <hengjchan@gmail.com>
2023-11-26 11:20:46 +0800

    working on styling with tailwind

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -1,4 +1,5 @@
 import { formatCurrency } from '../../utils/helpers';
+import Button from '../../ui/Button';
 
 type Props = {
   item: {
@@ -7,18 +8,19 @@ type Props = {
     quantity: number;
     totalPrice: number;
   };
-}
+};
 
 function CartItem({ item }: Props) {
   const { name, quantity, totalPrice } = item;
 
   return (
-    <li>
-      <p>
+    <li className="py-3 sm:flex sm:items-center sm:justify-between">
+      <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
+      <div className="flex items-center justify-between sm:gap-6">
+        <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <Button type="small">Delete</Button>
       </div>
     </li>
   );
