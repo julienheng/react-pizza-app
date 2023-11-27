75ef6f173bc9e40925326de1944e3593341deb04
Julien <hengjchan@gmail.com>
2023-11-27 16:00:23 +0800

    successfully fixed all the errors

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -17,11 +17,11 @@ export default function Button({
   onClick,
 }: Props) {
   const base =
-    'inline-block text-sm rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    'inline-block text-sm rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-200 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 px-4 md:px-6 focus:ring-offset-2 disabled:cursor-not-allowed';
 
   const styles: { [key: string]: string } = {
-    primary: base + 'px-4 py-3 sm:px-6 sm:py-4',
-    small: base + 'px-4 py-2 md:px-5 md:py-2.5  text-xs',
+    primary: base + 'px-6 py-3 sm:px-6 sm:py-4',
+    small: base + 'px-6 py-2 md:px-5 md:py-2.5 text-xs',
     secondary:
       'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-200 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
     round: base + 'px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
