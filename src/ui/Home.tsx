93ac1f2df0237621cfb936600c787d9cdb0088fb
Julien <hengjchan@gmail.com>
2023-11-27 16:07:13 +0800

    successfully fixed all the errors

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -7,22 +7,24 @@ function Home() {
   const username = useSelector((state: any) => state.user.username);
 
   return (
-    <div className="my-10 px-4 text-center sm:my-16">
-      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
-        The best pizza.
-        <br />
-        <span className="text-yellow-500">
-          Straight out of the oven, straight to you.
-        </span>
-      </h1>
+    <div className="border border-red-500">
+      <div className="my-10  px-4 text-center sm:my-16">
+        <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+          The best pizza.
+          <br />
+          <span className="text-yellow-500">
+            Straight out of the oven, straight to you.
+          </span>
+        </h1>
 
-      {username === '' ? (
-        <CreateUser />
-      ) : (
-        <Button  to="/menu" type="primary">
-          Continue Ordering, {username}
-        </Button>
-      )}
+        {username === '' ? (
+          <CreateUser />
+        ) : (
+          <Button to="/menu" type="primary">
+            Continue Ordering, {username}
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
